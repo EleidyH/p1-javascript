f849fa4db444fcc80a9f1dd7d515ebd5de2deb5e
EleidyH <eleidyhoyer@hotmail.com>
2023-11-03 09:01:13 +0100

    opdr 8.1

---
+++ Java_opdr_8.1/8.2/code.js
@@ -0,0 +1,46 @@
+function GetValues() {
+    Value1 = parseInt(document.getElementById("Value1").value);
+    Value2 = parseInt(document.getElementById("Value2").value);
+}
+
+if (Value1 <= 0 || Value2 <=0) {
+    SetResult("Het getal dat is ingevoerd is te laag!");
+    return false;
+}
+
+else {
+    return true;
+}
+
+function Multiply() {
+    if (GetValues()) {
+        Result = Value1 * Value2;
+        SetResult(Result);
+    }
+}
+
+function Subtract() {
+    if (GetValues()) {
+        Result = Value1 - Value2;
+        SetResult(Result);
+    }   
+}
+
+function Sum() {
+    if (GetValues()) {
+        Result = Value1 + Value2;
+        SetResult(Result);
+    }
+}
+
+function Divide() {
+    if (GetValues()) {
+        Result = Value1 / Value2;
+        let rounded = Result.toFixed(1);
+        SetResult(rounded);
+    }
+}
+
+function SetResult(Result) {
+    Result.innerHTML = "Result: " + Result;
+}
